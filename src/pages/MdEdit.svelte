54d6d54bb370250c05f04f0ae9f97b435449d046
Khan Kibria <khan.kibria@gmail.com>
2020-04-24 15:23:33 -0700

    update

--- src/pages/MdEdit.svelte
+++ src/pages/MdEdit.svelte
@@ -1,33 +1,48 @@
 <script>
-  import marked from "marked";
+  import { mdInit, clearMap, testSnippet, syncResultScroll, syncSrcScroll } from "../mdinit";
 
-  let source = `
-# H1 heading
+  let source = testSnippet;
+  let markdown;
 
-## H2 heading
+  let srcEl;
+  let rsltEl;
 
-### H3 heading
+  var mdHtml = mdInit("c-src", "c-html");
 
----
+  let timer, timerSrc, timerRslt;
+  let srcScroll = true;
 
-**bold text**
+  $: updateResult(source);
 
-*italicized text*
+  const updateResult = src => {
+    markdown = mdHtml.render(src);
+    clearMap();
+  };
 
----
+  const debounce = e => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      source = e.target.value;
+    }, 300);
+  };
 
-1. First item
-2. Second item
-3. Third item
+  const handleSrcScroll = e => {
+    if (!srcScroll) {
+      clearTimeout(timerSrc);
+      timerSrc = setTimeout(() => {
+        syncResultScroll();
+      }, 50);
+    }
+  }
 
-- First item
-- Second item
-- Third item
-
-[Svelte](https://svelte.dev/)
-`;
-
-  $: markdown = marked(source);
+  const handleRsltScroll = e => {
+    if (!srcScroll) {
+      clearTimeout(timerRslt);
+      timerRslt = setTimeout(() => {
+        syncSrcScroll(e);
+      }, 50);
+    }
+  }
 </script>
 
 <style>
@@ -49,6 +64,9 @@
   }
 </style>
 
+<svelte:window
+  on:resize={clearMap} />
+
 <div class="c-page h-full">
   <header class="c-header text-center font-bold">
     <h1>Content editor</h1>
@@ -57,12 +75,24 @@
   <div class="c-content flex">
     <div class="c-md w-1/2">
       <textarea
+        on:keyup paste cut mouseup={debounce}
+        on:scroll={handleSrcScroll}
+        on:touchstart mouseover={function(e) {
+          srcScroll = true;
+        }}
         bind:value={source}
-        class="c-panel border-black-500 bg-gray-200" />
+        bind:this={srcEl}
+        class="c-src c-panel border-black-500 bg-gray-200" />
     </div>
 
     <div class="w-1/2">
-      <div class="c-html c-panel border-black-500">
+      <div
+        on:scroll={handleRsltScroll }
+        on:touchstart mouseover={function(e) {
+          srcScroll = false;
+        }}
+        bind:this={rsltEl}
+        class="c-html c-panel border-black-500">
         {@html markdown}
       </div>
     </div>
